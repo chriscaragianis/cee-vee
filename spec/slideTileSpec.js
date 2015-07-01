764fa5ff8b2d03856d885da1c5e86476154734de
Chris Caragianis <chris.caragianis@gmail.com>
2015-06-30 22:36:13 -0400

    Only swiping one way for now

--- spec/slideTileSpec.js
+++ spec/slideTileSpec.js
@@ -10,6 +10,7 @@ describe("SlideTile", function() {
   }); 
   it ("brings up the next tile to the left on click left", function () {
     resetStubs();
+    $('.left-arrow').click(); 
     expect($('#dummy-tile-1-1').css('display')).toEqual('none'); 
     expect($('#dummy-tile-1-2').css('display')).toEqual('none'); 
     expect($('#dummy-tile-1-3').css('display')).toEqual('block'); 
@@ -17,8 +18,9 @@ describe("SlideTile", function() {
     expect($('#dummy-tile-2-2').css('display')).toEqual('none'); 
     expect($('#dummy-tile-2-3').css('display')).toEqual('block');
   }); 
-  it ("brings up the next tile to the left on swipe left", function () {
+  it ("brings up the next tile to the left on swipe", function () {
     resetStubs();
+    $('.slide-tile').swipe(); 
     expect($('#dummy-tile-1-1').css('display')).toEqual('none'); 
     expect($('#dummy-tile-1-2').css('display')).toEqual('none'); 
     expect($('#dummy-tile-1-3').css('display')).toEqual('block'); 
@@ -28,15 +30,7 @@ describe("SlideTile", function() {
   }); 
   it ("brings up the next tile to the right on click right", function () {
     resetStubs();
-    expect($('#dummy-tile-1-1').css('display')).toEqual('none'); 
-    expect($('#dummy-tile-1-2').css('display')).toEqual('block'); 
-    expect($('#dummy-tile-1-3').css('display')).toEqual('none'); 
-    expect($('#dummy-tile-2-1').css('display')).toEqual('none'); 
-    expect($('#dummy-tile-2-2').css('display')).toEqual('block'); 
-    expect($('#dummy-tile-2-3').css('display')).toEqual('none');
-  }); 
-  it ("brings up the next tile to the right on swipe right", function () {
-    resetStubs();
+    $('right.arrow').click(); 
     expect($('#dummy-tile-1-1').css('display')).toEqual('none'); 
     expect($('#dummy-tile-1-2').css('display')).toEqual('block'); 
     expect($('#dummy-tile-1-3').css('display')).toEqual('none'); 
