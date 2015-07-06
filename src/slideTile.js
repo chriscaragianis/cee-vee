9f52887bd2b9c175dc652eacc73a58f1ba45bd50
Chris Caragianis <chriscaragianis@gmail.com>
2015-07-06 16:40:12 -0400

    installed slide tiles

--- src/slideTile.js
+++ src/slideTile.js
@@ -8,6 +8,14 @@ $('.two').on('click', function() {
 });
 $('.three').on('click', function() {
   $($(this).parent().siblings()).hide(200);
+  $($(this).parent().siblings()[2]).show(200);
+});
+$('.four').on('click', function() {
+  $($(this).parent().siblings()).hide(200);
   $($(this).parent().siblings()[3]).show(200);
+});
+$('.five').on('click', function() {
+  $($(this).parent().siblings()).hide(200);
+  $($(this).parent().siblings()[4]).show(200);
 });
 
