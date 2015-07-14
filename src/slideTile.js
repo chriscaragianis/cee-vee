4d75f1d59260037ea31e864286d05962444d978f
Chris Caragianis <chris.caragianis@gmail.com>
2015-07-14 11:32:08 -0400

    Finishing touches, take 1

--- src/slideTile.js
+++ src/slideTile.js
@@ -1,21 +1,21 @@
 $('.one').on('click', function() {
-  $($(this).parent().siblings()).hide(200);
-  $($(this).parent().siblings()[0]).show(200);
+  $($(this).parent().siblings()).hide();
+  $($(this).parent().siblings()[0]).fadeIn(200);
 });
 $('.two').on('click', function() {
-  $($(this).parent().siblings()).hide(200);
-  $($(this).parent().siblings()[1]).show(200);
+  $($(this).parent().siblings()).hide();
+  $($(this).parent().siblings()[1]).fadeIn(200);
 });
 $('.three').on('click', function() {
-  $($(this).parent().siblings()).hide(200);
-  $($(this).parent().siblings()[2]).show(200);
+  $($(this).parent().siblings()).hide();
+  $($(this).parent().siblings()[2]).fadeIn(200);
 });
 $('.four').on('click', function() {
-  $($(this).parent().siblings()).hide(200);
-  $($(this).parent().siblings()[3]).show(200);
+  $($(this).parent().siblings()).hide();
+  $($(this).parent().siblings()[3]).fadeIn(200);
 });
 $('.five').on('click', function() {
-  $($(this).parent().siblings()).hide(200);
-  $($(this).parent().siblings()[4]).show(200);
+  $($(this).parent().siblings()).hide();
+  $($(this).parent().siblings()[4]).fadeIn(200);
 });
 
