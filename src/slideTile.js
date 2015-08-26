42961eb31675bb9ee18f1ea838b157c0a75dd3cf
Chris Caragianis <chriscaragianis@gmail.com>
2015-08-26 17:15:28 -0400

    Added experience row

--- src/slideTile.js
+++ src/slideTile.js
@@ -1,15 +1,31 @@
 $('.CV-control').on('click', function() {
   $('.AC').hide();
   $('.PJ').hide();
-  $('.CV').show();
+  $('.CV').fadeIn(200);
 });
 $('.AC-control').on('click', function() {
   $('.CV').hide();
   $('.PJ').hide();
-  $('.AC').show();
+  $('.AC').fadeIn(200);
 });
 $('.PJ-control').on('click', function() {
   $('.CV').hide();
   $('.AC').hide();
-  $('.PJ').show();
+  $('.PJ').fadeIn(200);
+});
+
+$('.TEACH-control').on('click', function() {
+  $('.SUM').hide();
+  $('.MATH').hide();
+  $('.TEACH').fadeIn(200);
+});
+$('.SUM-control').on('click', function() {
+  $('.TEACH').hide();
+  $('.MATH').hide();
+  $('.SUM').fadeIn(200);
+});
+$('.MATH-control').on('click', function() {
+  $('.TEACH').hide();
+  $('.SUM').hide();
+  $('.MATH').fadeIn(200);
 });
