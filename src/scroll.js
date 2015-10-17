51d1b46188a8a1f8df5ce4d9f6630dfce1d9f43d
Chris Caragianis <chriscaragianis@gmail.com>
2015-10-17 10:57:04 -0400

    Fix long lines and whitspace, remove unneccesary trs

--- src/scroll.js
+++ src/scroll.js
@@ -1,36 +1,36 @@
 $('#who').on('click', function() {
   $('html, body').animate({
     scrollTop: $('#whoami').offset().top - 50 
-    }, 1000);
+    }, 400);
 });
 
 $('#exp').on('click', function() {
   $('html, body').animate({
     scrollTop: $('#experience').offset().top - 50 
-    }, 1000);
+    }, 400);
 });
 
 $('#edu').on('click', function() {
   $('html, body').animate({
     scrollTop: $('#education').offset().top - 50
-    }, 1000);
+    }, 400);
 });
 
 $('#port').on('click', function() {
   $('html, body').animate({
     scrollTop: $('#portfolio').offset().top - 50
-    }, 1000);
+    }, 400);
 });
 
 $('#cc').on('click', function() {
   $('html, body').animate({
     scrollTop: $('#clean-code').offset().top - 50
-    }, 1000);
+    }, 400);
 });
 
 $('#git').on('click', function() {
   $('html, body').animate({
     scrollTop: $('#contact').offset().top - 50
-    }, 1000);
+    }, 400);
 });
 
